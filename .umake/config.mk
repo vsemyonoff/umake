33285bd4bb2746fbe0084d37ab1a0f04afdc2c38
Vladyslav Semyonov <vsemyonoff@gmail.com>
2011-11-14 12:58:13 +0200

    Default compiler flags updated

--- .umake/config.mk
+++ .umake/config.mk
@@ -58,7 +58,7 @@ CPPINCPATH   = include
 # Compilers settings
 #
 # C compiler flags
-CFLAGS       = -ggdb -pipe -pedantic -Wall
+CFLAGS       = -g -ggdb3 -pipe -pedantic -Wall -Werror -O0
 # C++ compiler flags
 CXXFLAGS     = $$(CFLAGS)
 
